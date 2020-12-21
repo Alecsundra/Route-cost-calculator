3bc106cba7ff2ea63a33846dee442aedc4903dae
Sandra <alecsandra.serediuc@gmail.com>
2020-12-21 12:05:39 +0100

    Form and submit function on calculating the total price

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { 
+    Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
+const FormD = () => {
+    const [distance, setDistance] = useState('');
+    const [price, setPrice] = useState('');
+    const [total, setTotal] =useState('');
 
-const Form = () => {
+const getTotalPrice = () =>{
+    setTotal(distance*price)
+    //letting the form line clean
+    setDistance('')
+    setPrice('')
+}
+console.log(total)
   return (
-    <div className="Form">
-      Form
-
-    </div>
+    <Form>
+      <FormGroup>
+        <Label for="exampleNumber">Distance(km)</Label>
+        <Input
+          value={distance}
+          onChange={e => setDistance(e.target.value)}
+          type="number"
+          name="distance"
+          id="distance"
+          placeholder="Distance in km"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleNumber">Price($/km)</Label>
+        <Input
+          value={price}
+          onChange={e =>setPrice(e.target.value)}
+          type="number"
+          name="price"
+          id="price"
+          placeholder="Price $/km"
+        />
+      </FormGroup>
+      {/* <FormGroup>
+        <Label for="exampleSelect">Select</Label>
+        <Input type="select" name="select" id="exampleSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Input>
+      </FormGroup> */}
+      <Button onClick={getTotalPrice}>Submit</Button>
+      <h4>Total price:{total}$</h4>
+    </Form>
   );
 }
 
-export default Form;
+export default FormD;
